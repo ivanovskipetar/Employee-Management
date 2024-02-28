cbd17b580c2db6c43c1f75e7f4c540246948c9a9
ivanovskipetar <ivanovskipetar4@gmail.com>
2024-02-28 15:17:27 +0100

    Configured Routing.

--- springboot-rest-api/ems-frontend/src/App.jsx
+++ springboot-rest-api/ems-frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 import ListEmployeeComponent from "./components/ListEmployeeComponent.jsx";
 import HeaderComponent from "./components/HeaderComponent.jsx";
 import FooterComponent from "./components/FooterComponent.jsx";
 
 function App() {
 
-  return (
-    <>
-        <HeaderComponent/>
-        <ListEmployeeComponent />
-        <FooterComponent/>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <HeaderComponent/>
+                <Routes>
+                    <Route path='/' element={<ListEmployeeComponent/>}></Route>
+                    <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+                </Routes>
+                <FooterComponent/>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
