b4f1231a024c3d8a157c6938aa624fa7d931267d
ivanovskipetar <ivanovskipetar4@gmail.com>
2024-02-29 22:41:22 +0100

    Connected React App to Add Employee REST API.

--- springboot-rest-api/ems-frontend/src/services/EmployeeService.js
+++ springboot-rest-api/ems-frontend/src/services/EmployeeService.js
@@ -5,3 +5,6 @@ const REST_API_BASE_URL = 'http://localhost:8080/api/employees';
 export const listEmployees = () => {
     return axios.get(REST_API_BASE_URL + '/get')
 }
+export const createEmployee = (employee) => {
+    return axios.post(REST_API_BASE_URL + '/add', employee)
+}
